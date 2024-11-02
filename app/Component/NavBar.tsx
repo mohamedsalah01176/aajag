489947d17a5a219242c83637cc7445341b52ed4b
Mohamed <kahledamam@gmail.com>
2024-11-02 07:59:22 +0200

    modify error

--- app/Component/NavBar.tsx
+++ app/Component/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from 'next/image'
+import Link from 'next/link';
 import { useState } from 'react';
 import { FaBars } from "react-icons/fa";
 // import FloateNav from './FloateNav';
@@ -15,23 +16,47 @@ export default function NavBar() {
             <h1> جمعية الإعجاز العلمى   </h1>
         </div>
         <ul className=' gap-5 hidden lg:flex text-[23px]'>
-            <a href='/' className='hover:scale-110 transition-all duration-300'>الرئيسية</a>
-            <a href='#' className='hover:scale-110 transition-all duration-300'>المؤتمرات</a>
-            <a href='#' className='hover:scale-110 transition-all duration-300'>القوائم</a>
-            <a href='#' className='hover:scale-110 transition-all duration-300'>الملتقيات وورش العمل</a>
-            <a href='#' className='hover:scale-110 transition-all duration-300'>من نحن </a>
-            <a href='#' className='hover:scale-110 transition-all duration-300'>التواصل</a>
+            <li>
+                <Link href='/' className='hover:scale-110 transition-all duration-300'>الرئيسية</Link>
+            </li>
+            <li>
+            <Link href='#' className='hover:scale-110 transition-all duration-300'>المؤتمرات</Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>القوائم</Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>الملتقيات وورش العمل</Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>من نحن </Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>التواصل</Link>
+            </li>
         </ul>
 
         <FaBars className='text-2xl font-bold cursor-pointer lg:hidden hover:scale-125 transition-all duration-300' onClick={()=>setOpen(!open)}/>
             <div className={` ${open?'opacity-1':'opacity-0 -z-20'} border-[20px] border-green-700 absolute top-[90px] left-6 border-t-transparent border-r-transparent border-l-transparent `}></div>
         <ul className={`${open?'opacity-1':'opacity-0 -z-20'} text-[23px] gap-5 flex flex-col  absolute top-[130px] left-4 bg-green-700  min-w-[300px] rounded-lg text-center p-3`}>
-            <a href='/' className=' hover:scale-110 transition-all duration-300'>الرئيسية</a>
-            <a href='#' className=' hover:scale-110 transition-all duration-300'>المؤتمرات</a>
-            <a href='#' className=' hover:scale-110 transition-all duration-300'>القوائم</a>
-            <a href='#' className=' hover:scale-110 transition-all duration-300'>الملتقيات وورش العمل</a>
-            <a href='#' className=' hover:scale-110 transition-all duration-300'>من نحن </a>
-            <a href='#' className=' hover:scale-110 transition-all duration-300'>التواصل</a>
+        <li>
+                <Link href='/' className='hover:scale-110 transition-all duration-300'>الرئيسية</Link>
+            </li>
+            <li>
+            <Link href='#' className='hover:scale-110 transition-all duration-300'>المؤتمرات</Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>القوائم</Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>الملتقيات وورش العمل</Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>من نحن </Link>
+            </li>
+            <li>
+                <Link href='#' className='hover:scale-110 transition-all duration-300'>التواصل</Link>
+            </li>
         </ul>
     </div>
   )
